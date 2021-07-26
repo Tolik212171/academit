fa0369173b980e598c16077f8be3d2904f84bd90
tolik212171 <Tolik212171@gmail.com>
2021-07-26 21:52:28 +0700

    Реализована финальная версия задачи Range*

--- Range/src/ru/academits/ilchenko/range/Range.java
+++ Range/src/ru/academits/ilchenko/range/Range.java
@@ -9,20 +9,20 @@ public class Range {
         this.to = to;
     }
 
-    public void setFrom(double from) {
-        this.from = from;
-    }
-
-    public void setTo(double to) {
-        this.to = to;
-    }
-
     public double getFrom() {
         return from;
     }
 
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
     public double getTo() {
         return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
     }
 
     public double getLength() {
@@ -38,14 +38,34 @@ public class Range {
             return null;
         }
 
-        return new Range(Math.min(range.from, from), Math.max(range.to, to));
+        return new Range(Math.max(range.from, from), Math.min(range.to, to));
     }
 
-    public Range[] getCombiningIntervals(Range range) {
+    public Range[] getUnion(Range range) {
         if (range.to < from || range.from > to) {
             return new Range[]{new Range(range.from, range.to), new Range(from, to)};
         }
 
         return new Range[]{new Range(Math.min(range.from, from), Math.max(range.to, to))};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (range.from > from && range.to >= to && range.from < to) {
+            return new Range[]{new Range(from, range.from)};
+        }
+
+        if (range.from > from && range.to < to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        }
+
+        if (range.from <= from && range.to < to && range.to > from) {
+            return new Range[]{new Range(range.to, to)};
+        }
+
+        if (range.from >= to || range.to <= from) {
+            return new Range[]{this};
+        }
+
+        return new Range[]{};
     }
 }
--- Range/src/ru/academits/ilchenko/range_main/RangeMain.java
+++ Range/src/ru/academits/ilchenko/range_main/RangeMain.java
@@ -7,34 +7,49 @@ import java.util.Scanner;
 public class RangeMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Range range = new Range(4, 10);
+        Range range1 = new Range(4, 10);
 
-        System.out.println("Диапазон начинается с " + range.getFrom());
-        System.out.println("Диапазон заканчивается " + range.getTo());
-        System.out.println("Длина диапазона: " + range.getLength());
+        System.out.println("Диапазон начинается с " + range1.getFrom());
+        System.out.println("Диапазон заканчивается " + range1.getTo());
+        System.out.println("Длина диапазона: " + range1.getLength());
 
         System.out.println("Введите число для проверки вхождения в диапазон:");
         double checkedNumber = scanner.nextDouble();
 
-        System.out.println("Результат проверки " + range.isInside(checkedNumber));
+        System.out.println("Результат проверки " + range1.isInside(checkedNumber));
 
-        range.setFrom(5);
-        range.setTo(22);
+        range1.setFrom(5);
+        range1.setTo(22);
 
-        System.out.println("Новый диапазон от " + range.getFrom() + " до " + range.getTo());
+        System.out.println("Новый диапазон от " + range1.getFrom() + " до " + range1.getTo());
 
-        range = range.getIntersection(new Range(2, 26));
-        System.out.println("Новый диапазон от " + range.getFrom() + " до " + range.getTo());
+        range1 = range1.getIntersection(new Range(2, 26));
+        System.out.println("Новый диапазон от " + range1.getFrom() + " до " + range1.getTo());
 
-        Range[] array = range.getCombiningIntervals(new Range(27, 35));
+        Range[] array1 = range1.getUnion(new Range(27, 35));
 
-        if (array.length == 1) {
-            System.out.println("Результат пересечения диапазонов, новый диапазон от "
-                    + array[0].getFrom() + " до " + array[0].getTo());
+        if (array1.length == 1) {
+            System.out.println("Результат объединения диапазонов, новый диапазон от "
+                    + array1[0].getFrom() + " до " + array1[0].getTo());
         } else {
-            for (int i = 0; i < array.length; i++) {
-                System.out.println("Результат пересечения диапазонов, новый диапазон " + (i + 1) + " куска " + "от "
-                        + array[i].getFrom() + " до " + array[i].getTo());
+            for (int i = 0; i < array1.length; i++) {
+                System.out.println("Результат объединения диапазонов, новый диапазон " + (i + 1) + " куска " + "от "
+                        + array1[i].getFrom() + " до " + array1[i].getTo());
+            }
+        }
+
+        Range range2 = new Range(1, 7);
+        Range[] array2 = range2.getDifference(new Range(3, 5));
+
+        if (array2.length == 0) {
+            System.out.println("Разность дала нулевой результат");
+        } else if (array2.length == 1) {
+            System.out.println("Результат разности, новый диапазон:                                                                        й диапазон от "
+                    + array2[0].getFrom() + " до " + array2[0].getTo());
+        } else {
+            for (int i = 0; i < array1.length; i++) {
+                System.out.println("Результат разности, новый диапазон: " + (i + 1) + " куска " + "от "
+                        + array2[i].getFrom() + " до " + array2[i].getTo());
             }
         }
     }
