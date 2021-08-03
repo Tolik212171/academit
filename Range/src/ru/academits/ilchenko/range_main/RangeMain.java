945860dd1da4dca1134b64aca7011cf6669dfa40
Ilchenko Anatolij <a.ilchenko@cft.ru>
2021-08-03 15:59:12 +0700

    Улучшен формат печати

--- Range/src/ru/academits/ilchenko/range_main/RangeMain.java
+++ Range/src/ru/academits/ilchenko/range_main/RangeMain.java
@@ -21,36 +21,51 @@ public class RangeMain {
         range1.setFrom(5);
         range1.setTo(22);
 
-        System.out.println("Новый диапазон от " + range1.getFrom() + " до " + range1.getTo());
+        printRange(range1);
 
-        range1 = range1.getIntersection(new Range(2, 26));
-        System.out.println("Новый диапазон от " + range1.getFrom() + " до " + range1.getTo());
+        Range rangeIntersection = range1.getIntersection(new Range(2, 26));
+        printRange(rangeIntersection);
 
         Range[] array1 = range1.getUnion(new Range(27, 35));
 
         if (array1.length == 1) {
-            System.out.println("Результат объединения диапазонов, новый диапазон от "
-                    + array1[0].getFrom() + " до " + array1[0].getTo());
+            System.out.println("Результат объединения диапазонов: ");
         } else {
-            System.out.println("Результат объединения диапазонов, новые диапазоны:");
-            for (int i = 0; i < array1.length; i++) {
-                System.out.println((i + 1) + ") " + array1[i].toString());
-            }
+            System.out.println("Результат объединения диапазонов:");
         }
+        printRange(array1);
 
         Range range2 = new Range(1, 7);
-        Range[] array2 = range2.getDifference(new Range(3, 5));
+        Range[] array2 = range2.getDifference(new Range(1, 5));
 
         if (array2.length == 0) {
-            System.out.println("Разность дала нулевой результат");
+            System.out.println("Разность дала нулевой результат:");
+            printRange(array2);
         } else if (array2.length == 1) {
-            System.out.println("Результат разности, новый диапазон:"
-                    + array2[0].toString());
+            System.out.println("Результат разности:");
+            printRange(array2);
         } else {
-            System.out.println("Результат разности, новые диапазоны:");
-            for (int i = 0; i < array1.length; i++) {
-                System.out.println((i + 1) + ") " + array2[i].toString());
-            }
+            System.out.println("Результат разности:");
+            printRange(array2);
         }
+    }
+
+    static void printRange(Range[] array1) {
+        if (array1.length == 0) {
+            System.out.println("[]");
+        } else {
+            System.out.print("[");
+            for (int i = 0; i < array1.length; i++) {
+                System.out.print((array1[i].toString()));
+                if (i < array1.length - 1) {
+                    System.out.print(",");
+                }
+            }
+            System.out.println("]");
+        }
+    }
+
+    static void printRange(Range range) {
+        System.out.println("Новый диапазон (" + range.getFrom() + ";" + range.getTo() + ")");
     }
 }
